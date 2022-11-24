ab9ff7f37f2911cdcd30dc1a4e011e0f3b5d65d4
Kelxety <85091831+Kelxety@users.noreply.github.com>
2022-11-25 00:45:45 +0800

    added vercel url base

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,7 +1,9 @@
 import { Experience } from "../typings";
+import { fetchBaseUrl } from "./fetchBaseUrl";
 
-export const fetchExperiences = async() => {
-   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
+export const fetchExperiences = async () => {
+   
+   const res = await fetch(`${fetchBaseUrl()}/api/getExperience`);
 
    const data = await res.json()
    const experiences: Experience[] = data.experiences
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,7 +1,8 @@
 import { Social } from "../typings";
+import { fetchBaseUrl } from "./fetchBaseUrl";
 
 export const fetchSocials = async() => {
-   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
+   const res = await fetch(`${fetchBaseUrl()}/api/getSocials`);
 
    const data = await res.json()
    const socials: Social[] = data.socials
