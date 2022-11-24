a44e80d0c9916d7970ca1dbf595d578b9429e01e
Kelxety <85091831+Kelxety@users.noreply.github.com>
2022-11-25 00:32:30 +0800

    try

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type { GetStaticProps, NextPage } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import { Experience, PageInfo, Project, Skill, Social } from '../typings';
-import { fetchExperiences } from '../utils/fetchExperiences';
-import { fetchPageInfo } from '../utils/fetchPageInfo';
-import { fetchProjects } from '../utils/fetchProjects';
-import { fetchSkills } from '../utils/fetchSkills';
-import { fetchSocials } from '../utils/fetchSocials';
-import About from './components/About';
-import ContactMe from './components/ContactMe';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import WorkExperience from './components/WorkExperience';
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchSocials } from "../utils/fetchSocials";
+import About from "./components/About";
+import ContactMe from "./components/ContactMe";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import WorkExperience from "./components/WorkExperience";
 
 interface Props {
   pageInfo: PageInfo;
@@ -24,65 +24,68 @@ interface Props {
 }
 
 const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
-
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 md:scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-red-500/80'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 md:scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-red-500/80">
       <Head>
-        <title>{pageInfo && (<p>{pageInfo?.name} {" "}</p>)} Portfolio</title>
+        <title>{pageInfo && <p>{pageInfo?.name} </p>} Portfolio</title>
         <meta name="description" content="This is my Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
-      <Header socials={socials}/>
+      {pageInfo && (
+        <>
+          {/* Header */}
+          <Header socials={socials} />
 
-      {/* Hero */}
-      <section id="hero" className='snap-center'>
-        <Hero pageInfo={pageInfo} />
-      </section>
-      
-      {/* About */}
-      <section id="about" className='snap-center'>
-        <About pageInfo={pageInfo} />
-      </section>
+          {/* Hero */}
+          <section id="hero" className="snap-center">
+            <Hero pageInfo={pageInfo} />
+          </section>
 
-      {/* Experience */}
-      <section id="experience" className='snap-center'>
-        <WorkExperience experiences={experiences} />
-      </section>
+          {/* About */}
+          <section id="about" className="snap-center">
+            <About pageInfo={pageInfo} />
+          </section>
 
-      
-      {/* Skills */}
-      <section id="skills" className="snap-start px-4">
-        <Skills skills={skills} />
-      </section>
+          {/* Experience */}
+          <section id="experience" className="snap-center">
+            <WorkExperience experiences={experiences} />
+          </section>
 
-      {/* Projects */}
-      <section id="projects" className="snap-start">
-        <Projects projects={projects} />
-      </section>
+          {/* Skills */}
+          <section id="skills" className="snap-start px-4">
+            <Skills skills={skills} />
+          </section>
 
-      {/* Contact Me */}
-      <section id="contact" className="snap-start">
-        <ContactMe />
-      </section>
+          {/* Projects */}
+          <section id="projects" className="snap-start">
+            <Projects projects={projects} />
+          </section>
 
+          {/* Contact Me */}
+          <section id="contact" className="snap-start">
+            <ContactMe />
+          </section>
 
-      <Link href="#hero">
-        <footer className='bottom-5 w-full cursor-pointer'>
-          <div className='flex items-center justify-center'>
-            <img
-              className='h-5 w-8 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
-              src={"/images/jrl.png"}
-              alt=""
-            />
-            <p className='text-xs p-4 text-white/50'>Copyright @ jrldev.programmer@gmail.com</p>
-          </div>
-        </footer>
-      </Link>
+          <Link href="#hero">
+            <footer className="bottom-5 w-full cursor-pointer">
+              <div className="flex items-center justify-center">
+                <img
+                  className="h-5 w-8 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+                  src={"/images/jrl.png"}
+                  alt=""
+                />
+                <p className="text-xs p-4 text-white/50">
+                  Copyright @ jrldev.programmer@gmail.com
+                </p>
+              </div>
+            </footer>
+          </Link>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
 
@@ -99,8 +102,8 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       experiences,
       skills,
       socials,
-      projects
+      projects,
     },
     revalidate: 60,
-  }
-}
+  };
+};
