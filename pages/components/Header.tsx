377c156f3332d984e9c09eca2ce2f772f8d7df9f
Kelxety <85091831+Kelxety@users.noreply.github.com>
2022-11-24 22:49:21 +0800

    test not to load when no data

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,69 +1,76 @@
-import React from 'react';
+import React from "react";
 import { SocialIcon } from "react-social-icons";
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { Social } from '../../typings';
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { Social } from "../../typings";
 
 type Props = {
-   socials: Social[]
-}
+  socials: Social[];
+};
 
-const Header = ({socials}: Props) => {
-   return (
-      <header className='sticky top-0 flex p-5 items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-         <motion.div className='flex flex-row items-center'
+const Header = ({ socials }: Props) => {
+  return (
+    <header className="sticky top-0 flex p-5 items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      {socials && (
+        <>
+          <motion.div
+            className="flex flex-row items-center"
             initial={{
-               y: -500,
-               opacity: 0,
-               scale: 0.5,
+              y: -500,
+              opacity: 0,
+              scale: 0.5,
             }}
             animate={{
-               y: 0,
-               opacity: 1,
-               scale: 1,
+              y: 0,
+              opacity: 1,
+              scale: 1,
             }}
             transition={{
-               duration:1.5
+              duration: 1.5,
             }}
-         >
+          >
             {/* Social Icons */}
             {socials?.map((social) => (
-               <SocialIcon
-                  key={social?._id}
-               url={social?.url}
-               fgColor='gray'
-               bgColor='transparent'
-            />
+              <SocialIcon
+                key={social?._id}
+                url={social?.url}
+                fgColor="gray"
+                bgColor="transparent"
+              />
             ))}
-         </motion.div>
-         <Link href="#contact">
+          </motion.div>
+          <Link href="#contact">
             <motion.div
-               initial={{
-                  y: -500,
-                  opacity: 0,
-                  scale: 0.5,
-               }}
-               animate={{
-                  y: 0,
-                  opacity: 1,
-                  scale: 1,
-               }}
-               transition={{
-                  duration:1.5
-               }}
-               className='flex flex-row items-center text-gray-300 cursor-pointer'
+              initial={{
+                y: -500,
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 1.5,
+              }}
+              className="flex flex-row items-center text-gray-300 cursor-pointer"
             >
-               <SocialIcon
-                  className='cursor-pointer'
-                  network='email'
-                  fgColor='gray'
-                  bgColor='transparent'
-               />
-               <p className='uppercase hidden md:inline-flex text-sm text-gray-300'>Contact Me</p>
+              <SocialIcon
+                className="cursor-pointer"
+                network="email"
+                fgColor="gray"
+                bgColor="transparent"
+              />
+              <p className="uppercase hidden md:inline-flex text-sm text-gray-300">
+                Contact Me
+              </p>
             </motion.div>
-         </Link>
-      </header>
-   )
-}
+          </Link>
+        </>
+      )}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
