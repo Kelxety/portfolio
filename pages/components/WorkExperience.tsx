ed53b06779d2e697c847cb1ee4dfd92223684406
Kelxety <85091831+Kelxety@users.noreply.github.com>
2022-11-25 16:33:16 +0800

    added padding to work experience

--- pages/components/WorkExperience.tsx
+++ pages/components/WorkExperience.tsx
@@ -11,13 +11,12 @@ const WorkExperience = ({experiences}:Props) => {
   return (
      <motion.div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-5xl px-2 md:px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-sm md:text-2xl'>Experience</h3>
-      <div className='w-full flex space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-red-500/80'>
+      <div className='w-full flex space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-red-500/80 p-10'>
           {experiences?.map(experience => (
             <ExperienceCard key={experience?._id} experience={experience}/>
           ))}
            {/* ExperienceCard */}
-          <div className='p-5'></div>
-         </div>
+         </div> 
      </motion.div>
     
   )
